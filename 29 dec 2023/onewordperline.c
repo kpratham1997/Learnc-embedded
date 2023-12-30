5814f2d1813fedac59b0346099b10a20a00c24a1
kpratham1997 <53686087+kpratham1997@users.noreply.github.com>
2023-12-30 15:22:41 +0530

    Add files via upload

---
+++ 29 dec 2023/onewordperline.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+
+int main(){
+
+int c,state;
+
+state=OUT;
+
+while((c=getchar())!=EOF){
+    if( c== '\n' || c == '\t' || c == ' '){
+        if(state==IN){
+            putchar('\n');
+            state=OUT;
+        }
+
+    }else{
+
+        putchar(c);
+        state=IN;
+
+    }
+
+
+
+}
+
+
+}
