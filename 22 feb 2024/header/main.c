ee2dc106b4e70702eeaf2bd38878a6c107abb8d6
kpratham1997 <53686087+kpratham1997@users.noreply.github.com>
2024-02-22 00:09:56 +0530

    Add files via upload

---
+++ 22 feb 2024/header/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "math_operations.h"
+
+
+
+int main(){
+
+int num1=10;
+int num2=2;
+
+printf("addition : %d\n",addition(num1,num2));
+printf("substraction : %d\n",substract(num1,num2));
+printf("mutiply : %d\n",multiply(num1,num2));
+printf("divide : %d\n",divide(num1,num2));
+printf("remainder : %d\n",remainders(num1,num2));
+
+
+}
